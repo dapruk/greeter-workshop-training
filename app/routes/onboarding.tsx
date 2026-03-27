5b208fe36b284102c5c226cf6904f4cf78cb2458
akmalfman <akmalfsalman2@gmail.com>
2026-03-27 09:34:18 +0700

    init routes

--- app/routes/onboarding.tsx
+++ app/routes/onboarding.tsx
@@ -1,13 +1,13 @@
-import type { Route } from "./+types/home";
+import type { Route } from "./+types/onboarding";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Onboarding" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export default function Home() {
+export default function Onboarding() {
   return <Welcome />;
 }
