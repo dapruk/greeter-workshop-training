88784755dec2b27021f9f5002140ee307438f4a8
dapruk <dapruks@gmail.com>
2026-03-27 14:22:27 +0700

    chore: jest config adjustments

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   moduleNameMapper: {
@@ -16,5 +14,3 @@ const config: Config = {
   },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
 };
-
-export default config;
