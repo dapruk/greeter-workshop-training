14d323e1c65a5227503222d81708dbdbcf235d60
dapruk <dapruks@gmail.com>
2026-03-27 09:51:31 +0700

    feat: added zod schema for forms

--- app/components/greeting/forms/user-form.tsx
+++ app/components/greeting/forms/user-form.tsx
@@ -1,7 +1,10 @@
+import { z } from "zod";
+
+const UserFormSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  gender: z.enum(["male", "female"]),
+});
+
 export function UserForm() {
-  return (
-    <div>
-      <p>Form</p>
-    </div>
-  );
+  return <div></div>;
 }
